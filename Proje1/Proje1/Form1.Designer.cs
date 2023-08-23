c9770eb27c9e1db140e61bf0daaeb844cdec6010
Melih Yıldız <117168245+L3VYE@users.noreply.github.com>
2023-08-23 16:38:16 +0300

    Add files via upload

---
+++ Proje1/Proje1/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Proje1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lb_saniye = new System.Windows.Forms.Label();
+            this.lb_puan = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(67, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(254, 198);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "baklavaci-akay-ogullari-bagcilar.jpg");
+            this.ımageList1.Images.SetKeyName(1, "10102725902386.jpg");
+            this.ımageList1.Images.SetKeyName(2, "10102726393906.jpg");
+            this.ımageList1.Images.SetKeyName(3, "10102726426674.jpg");
+            this.ımageList1.Images.SetKeyName(4, "693337929_tn50_0.jpg");
+            this.ımageList1.Images.SetKeyName(5, "10102729801778.jpg");
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(96, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 91);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ZAR AT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(420, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(234, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "İsmi Kontrol Et";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(377, 172);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(338, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(377, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Kalan Zaman";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(591, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Puan";
+            // 
+            // lb_saniye
+            // 
+            this.lb_saniye.AutoSize = true;
+            this.lb_saniye.Location = new System.Drawing.Point(377, 96);
+            this.lb_saniye.Name = "lb_saniye";
+            this.lb_saniye.Size = new System.Drawing.Size(19, 13);
+            this.lb_saniye.TabIndex = 4;
+            this.lb_saniye.Text = "60";
+            // 
+            // lb_puan
+            // 
+            this.lb_puan.AutoSize = true;
+            this.lb_puan.Location = new System.Drawing.Point(591, 96);
+            this.lb_puan.Name = "lb_puan";
+            this.lb_puan.Size = new System.Drawing.Size(13, 13);
+            this.lb_puan.TabIndex = 4;
+            this.lb_puan.Text = "0";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(395, 277);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(285, 134);
+            this.listBox1.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lb_puan);
+            this.Controls.Add(this.lb_saniye);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lb_saniye;
+        private System.Windows.Forms.Label lb_puan;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Proje1/Proje1/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proje1
+{
+    public partial class Form1 : Form
+    {
+        int saniye = 60;
+        int zar1;
+        int puan = 0;
+        string secili = " ";
+        string listbox_ici = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            zar1= r.Next(0,6);
+            pictureBox1.Image = ımageList1.Images[zar1];
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+            switch (zar1)
+            {
+                case 0:
+                    secili = "A";
+                    break;
+                case 1:
+                    secili = "B";
+                    break;
+                case 2:
+                    secili = "D";
+                    break;
+                case 3:
+                    secili = "E";
+                    break;
+                case 4:
+                    secili = "M";
+                    break;
+                case 5:
+                    secili = "S";
+                    break;
+
+
+            }
+            button2.Enabled = true;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            saniye--;
+            lb_saniye.Text = Convert.ToString(saniye);
+            if (saniye==0)
+            {
+                listBox1.Items.Clear();
+                lb_puan.Text = "0";
+                lb_saniye.Text = "60";
+                timer1.Enabled=false;
+                saniye = 60;
+                puan = 0;
+                button2.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text=="")
+            {
+                MessageBox.Show("Lütfen Kelime Yazınız!");
+                return;
+            }
+            string textbox_ici = textBox1.Text.ToLower();
+            if (textBox1.Text.Substring(0, 1) == secili || textBox1.Text.Substring(0, 1) == secili.ToLower()) ;
+            {
+                puan = puan + 1;
+                lb_puan.Text=Convert.ToString(puan);
+                listBox1.Items.Add(textbox_ici);
+            }
+            if (listbox_ici.Contains(textbox_ici+","))
+            {
+                listBox1.Items.Clear();
+                lb_puan.Text = "0";
+                lb_saniye.Text = "60";
+                timer1.Enabled = false;
+                saniye = 60;
+                puan = 0;
+                button2.Enabled = false;
+            }
+            if (listbox_ici=="")
+            {
+                listbox_ici = textbox_ici + ",";
+
+            }
+            else
+            {
+                listbox_ici = listbox_ici + textbox_ici + ",";
+
+            }
+            textBox1.Text = "";
+            textbox_ici = "";
+        }
+    }
+}
